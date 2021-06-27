19fad563b6d6fbfe734c3472f4c66d33d89e5c10
Srit20 <80943355+Srit20@users.noreply.github.com>
2021-06-27 17:32:43 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by Sritam Subudhi @{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript</h1>
+      <p>A web dev bootcamp on Javascript and React JS</p>
+    </div>
+  );
+}
+export default Info;
